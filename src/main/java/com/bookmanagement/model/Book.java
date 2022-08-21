634f306b576ab13c062dcf8915e73afe75229040
arthipriya <100372444+arthipriya075@users.noreply.github.com>
2022-08-21 19:24:12 +0530

    Add files via upload

---
+++ src/main/java/com/bookmanagement/model/Book.java
@@ -0,0 +1,72 @@
+package com.bookmanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String username;
+	private String authorName;
+	private String genre;
+	private double rating;
+	
+	public Book() {}
+	
+	public Book(Long id, String title, String username, String authorName, String genre, double rating) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.username = username;
+		this.authorName = authorName;
+		this.genre = genre;
+		this.rating = rating;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+}
